410af6585f3ccc4a989b6a14e18012a85c5fe756
Eduardo <edumeirelles@yahoo.com.br>
2021-06-05 02:04:34 -0300

    Melhoria Exercício 4

--- EstruturaSequencial/Exercício_04.py
+++ EstruturaSequencial/Exercício_04.py
@@ -2,49 +2,18 @@
 
 notas = []
 
-while True:
-    try:
-        n1 = float(input('Digite a nota do 1º bimestre: ').replace(',','.'))
-        while n1 > 10 or n1 < 0:
-            print('Nota inválida.')
-            n1 = float(input('Digite a nota do 1º bimestre: ').replace(',','.'))
-        notas.append(n1)
-    except ValueError:
-        print('Nota inválida.') 
-    else:
-        while True:
-            try:
-                n2 = float(input('Digite a nota do 2º bimestre: ').replace(',','.'))
-                while n2 > 10 or n2 < 0:
-                    print('Nota inválida.')
-                    n2 = float(input('Digite a nota do 2º bimestre: ').replace(',','.'))
-                notas.append(n2)
-            except ValueError:
+for i in range(1,5):
+    while True:
+        try:
+            x = float(input(f'Digite a nota do {i}º bimestre: ').replace(',','.'))
+            while x > 10 or x < 0:
                 print('Nota inválida.')
-            else:
-                while True:
-                    try:
-                        n3 = float(input('Digite a nota do 3º bimestre: ').replace(',','.'))
-                        while n3 > 10 or n3 < 0:
-                            print('Nota inválida.')
-                            n3 = float(input('Digite a nota do 3º bimestre: ').replace(',','.'))
-                        notas.append(n3)
-                    except ValueError:
-                        print('Nota inválida.')
-                    else:
-                        while True:
-                            try:
-                                n4 = float(input('Digite a nota do 4º bimestre: ').replace(',','.'))
-                                while n4 > 10 or n4 < 0:
-                                    print('Nota inválida.')
-                                    n4 = float(input('Digite a nota do 4º bimestre: ').replace(',','.'))
-                                notas.append(n4)
-                                media = sum(notas) / len(notas)
-                                print('Média =', format(media,'.1f').replace('.',','))
-                            except ValueError:
-                                print('Nota inválida.')
-                            else:
-                                break
-                        break
-                break
-        break
+                x = float(input(f'Digite a nota do {i}º bimestre: ').replace(',','.'))
+            notas.append(x)      
+        except ValueError:
+            print('Nota inválida.') 
+        else:
+            break
+
+media = sum(notas) / len(notas)
+print('Média =', format(media,'.1f').replace('.',','))
